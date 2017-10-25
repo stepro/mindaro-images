ac2b5b54883c17c061d2ae5e15a909f88fab666a
Stephen Provine <stephpr@microsoft.com>
2017-10-25 10:43:24 -0700

    Automatically determine sync target

--- sync/run.sh
+++ sync/run.sh
@@ -4,28 +4,70 @@ if [ -z "$POD_NAME" ]; then
   echo >&2 error: must specify POD_NAME environment variable
   exit 1
 fi
-if [ -z "$TARGET_CONTAINER" ]; then
-  echo >&2 error: must specify TARGET_CONTAINER environment variable
-  exit 1
+
+TARGET_CONTAINER=$(kubectl get pod $POD_NAME -o go-template='{{ index .metadata.labels "mindaro.io/target-container" }}')
+if [ "$TARGET_CONTAINER" == "<no value>" ]; then
+  TARGET_CONTAINER=
 fi
-if [ -z "$TARGET_DIR" ]; then
-  echo >&2 error: must specify TARGET_DIR environment variable
-  exit 1
+
+SYNC_TARGET=$(kubectl get pod $POD_NAME -o go-template='{{ index .metadata.annotations "mindaro.io/sync-target" }}')
+if [ "$SYNC_TARGET" == "<no value>" ]; then
+  SYNC_TARGET=
+fi
+
+if [ -n "$TARGET_CONTAINER" -a -z "$SYNC_TARGET" ]; then
+  while true; do
+    READY=$(kubectl get pod $POD_NAME -o go-template='{{ range .status.containerStatuses }}{{ if eq .name "'$TARGET_CONTAINER'" }}{{ .ready }}{{ end }}{{ end }}')
+    if [ "$READY" == "true" ]; then
+      break
+    fi
+    echo info: waiting for target container readiness...
+    sleep 1
+  done
+  HAS_FIND=$(kubectl exec 2>/dev/null $POD_NAME -c $TARGET_CONTAINER -- which find)
+  if [ -n "$HAS_FIND" ]; then
+    CONTEXT_MARKER=$(kubectl exec 2>/dev/null $POD_NAME -c $TARGET_CONTAINER -- sh -c 'PWD="$(pwd)"
+      while true; do
+        FOUND=$(find "$PWD" -name .mindaro)
+        if [ -n "$FOUND" ]; then
+          rm -f "$FOUND"
+          echo $FOUND
+          break
+        elif [ -n "$(echo $PWD | grep -o ^/$)" ]; then
+          break
+        fi
+        PWD=$(dirname "$PWD")
+      done')
+    if [ -n "$CONTEXT_MARKER" ]; then
+      SYNC_TARGET=$(dirname "$CONTEXT_MARKER")
+      echo info: automatically determined sync target
+    fi
+  fi
+fi
+
+if [ -z "$TARGET_CONTAINER" -o -z "$SYNC_TARGET" ]; then
+  echo info: target container and/or sync target are unknown\; sync is disabled
+else
+  echo info: target container is \"$TARGET_CONTAINER\"
+  echo info: sync target is \"$SYNC_TARGET\"
 fi
 
 dcwatch | while read action file; do
+  if [ -z "$TARGET_CONTAINER" -o -z "$SYNC_TARGET" ]; then
+    continue
+  fi
   DELETE=$(echo "$action" | grep -)
   if [ -n "$DELETE" ]; then
-    echo kubectl exec $POD_NAME -c $TARGET_CONTAINER -- rm -rf \"$TARGET_DIR/$file\"
-    kubectl exec $POD_NAME -c $TARGET_CONTAINER -- rm -rf "$TARGET_DIR/$file"
+    echo info: deleting \"$SYNC_TARGET/$file\"
+    kubectl exec $POD_NAME -c $TARGET_CONTAINER -- rm -rf "$SYNC_TARGET/$file"
   elif [ -d "$file" ]; then
-    echo kubectl exec $POD_NAME -c $TARGET_CONTAINER -- mkdir -p \"$TARGET_DIR/$file\"
-    kubectl exec $POD_NAME -c $TARGET_CONTAINER -- mkdir -p "$TARGET_DIR/$file"
+    echo info: ensuring directory \"$SYNC_TARGET/$file\" exists
+    kubectl exec $POD_NAME -c $TARGET_CONTAINER -- mkdir -p "$SYNC_TARGET/$file"
   else
     DIR=$(dirname "$file")
-    echo kubectl exec $POD_NAME -c $TARGET_CONTAINER -- mkdir -p \"$TARGET_DIR/$DIR\"
-    kubectl exec $POD_NAME -c $TARGET_CONTAINER -- mkdir -p "$TARGET_DIR/$DIR"
-    echo kubectl cp \"$file\" $POD_NAME:\"$TARGET_DIR/$file\" -c $TARGET_CONTAINER
-    kubectl cp "$file" $POD_NAME:"$TARGET_DIR/$file" -c $TARGET_CONTAINER
+    echo info: ensuring directory \"$SYNC_TARGET/$DIR\" exists
+    kubectl exec $POD_NAME -c $TARGET_CONTAINER -- mkdir -p "$SYNC_TARGET/$DIR"
+    echo info: copying file \"$file\" to \"$SYNC_TARGET/$file\"
+    kubectl cp "$file" $POD_NAME:"$SYNC_TARGET/$file" -c $TARGET_CONTAINER
   fi
 done
